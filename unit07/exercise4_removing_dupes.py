e0eaccab9b7c1ae7550ea0b2545cf8478283b5b5
Zmartt R. Caracol <zmarttbusiness@gmail.com>
2026-08-25 22:54:16 +0800

    Created the other folders and the current exercise

---
+++ unit07/exercise4_removing_dupes.py
@@ -0,0 +1,17 @@
+#Medium difficulty
+#So the rules are:
+#Remove duplicate values
+#no using of a set
+#preserving the first occurence order.
+#What is first occurence order?
+#The first duplicate value must be the one kept.
+
+
+random_items = [
+    "laptop", "smartphone", "headphones", "backpack",
+    "laptop", "coffee mug", "desk lamp", "notebook",
+    "smartphone", "water bottle", "running shoes", "sunglasses",
+    "backpack", "wrist watch", "notebook", "charging cable"
+]
+
+#TODO continue this exercise after I have rested

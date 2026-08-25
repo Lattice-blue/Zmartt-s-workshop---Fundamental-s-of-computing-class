4af36c446863e9dee8b427eeb77556613689a4e8
Zmartt R. Caracol <zmarttbusiness@gmail.com>
2026-08-25 21:23:17 +0800

    Finished exercise 1 of unit 06

--- unit06/exercises.py
+++ unit06/exercises.py
@@ -19,12 +19,15 @@
 
 # Exercises
 
-# 1. ASk fora  word an print its first character, last character, and length
-word = input("Enter word: ")
-length = len(word)
-first_char = word[0]
-last_char = word[length - 1]
+# 1. Ask for a  word an print its first character, last character, and length
+# word = input("Enter word: ")
+# length = len(word)
+# first_char = word[0]
+# last_char = word[length - 1]
 
-print(f"first character = {first_char}")
-print(f"last character = {last_char}")
-print(f"length = {length}")
+# print(f"first character = {first_char}")
+# print(f"last character = {last_char}")
+# print(f"length = {length}")
+
+#2. Ask for a sentence and print it in uppercase and lowercase
+
